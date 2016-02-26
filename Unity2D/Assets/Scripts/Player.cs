5477c698be7286c1a320ed6e5f14c89e3a7c3376
Sebastião dos Santos <sbt.santos@outlook.com>
2016-02-26 17:35:57 -0300

    Envio do projeto contendo alguns componentes
    No projeto já foi adicionado alguns sprites, já foi feito algumas
    animações do sprites, adicionado scripts para movimentar o personagem ,
    a cena do jogo e o material do chão e a configuração da animação do
    personagem se movendo

---
+++ Unity2D/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    public float velocidade;
+    public float forcaPulo;
+    private bool estaNoChao;
+    public Transform chaoVerificador;
+    public Transform spritePlayer;
+    private Animator animator;
+    public LayerMask piso;
+
+    //Tudo que ocorre quando o personagem e criado
+    void Start()
+    {
+        animator = spritePlayer.GetComponent<Animator>();
+    }
+
+    //Tudo que ocorre enquanto o personagem existe
+    void Update()
+    {
+        Movimentacao();
+    }
+
+    //Responsavel pela movimentacao do personagem
+    void Movimentacao()
+    {
+
+        //Seta no paramentro movimento, um valor 0 ou maior que 0. Ira ativar a condicao para mudar de animacao
+        animator.SetFloat("movimento", Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+
+        //Anda para a direita
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        //Anda para a esquerda
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+        //Verifica se esta no chao
+        estaNoChao = Physics2D.OverlapCircle(chaoVerificador.position, 0.2f, piso);
+        animator.SetBool("chao", estaNoChao);
+
+        //Responsavel pelo pulo
+        if (Input.GetButtonDown("Jump") && estaNoChao)
+        {
+            GetComponent<Rigidbody2D>().AddForce(transform.up * forcaPulo);
+        }
+    }
+}
+
